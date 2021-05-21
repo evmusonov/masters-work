b43e5eee2150b6adc483bb9260725afbe198a4f2
Evgeny Musonov <evmusonov@gmail.com>
2021-05-21 11:43:07 +0700

    edit: register

--- node-api/controllers/userController.js
+++ node-api/controllers/userController.js
@@ -96,7 +96,7 @@ exports.loginDataValidation = {
   }
 }
 
-exports.register = (req, res, next) => {
+exports.register = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
